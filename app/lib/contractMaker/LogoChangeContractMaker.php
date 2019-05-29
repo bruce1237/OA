db994724647077fc4564d0f7652da091a099e433
波波 <346003541@qq.com>
2019-05-29 09:39:56 +0800

    new contract

---
+++ app/lib/contractMaker/LogoChangeContractMaker.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019-05-28
+ * Time: 14:21
+ * Used:
+ */
+
+namespace App\Lib\contractMaker;
+
+
+class LogoChangeContractMaker {
+
+}
