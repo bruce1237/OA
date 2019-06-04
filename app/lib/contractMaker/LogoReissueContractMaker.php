5ca593a1881cb628073fca9c920740d367b32fc3
波波 <346003541@qq.com>
2019-06-04 15:22:10 +0800

    format

--- app/lib/contractMaker/LogoReissueContractMaker.php
+++ app/lib/contractMaker/LogoReissueContractMaker.php
@@ -1,32 +1,33 @@
 <?php
 namespace App\Lib\contractMaker;
 
-class LogoReissueContractMaker extends ContractMaker{
-    
+class LogoReissueContractMaker extends ContractMaker
+{
+
     protected function restructureCarts($cartObj)
     {
         $orderDetailArray = [
             'type' => [],
             'name' => [],
             'attr' => [],
-            'price'=> [],
-            'total'=> 0,
+            'price' => [],
+            'total' => 0,
         ];
-        
+
         foreach ($cartObj as $cart) {
-            $attrs = json_decode($cart->service_attributes,true);
-            $attrStr ='';
+            $attrs = json_decode($cart->service_attributes, true);
+            $attrStr = '';
             foreach ($attrs as $attr) {
-                $attrStr .=$attr['name'].": ".$attr['value'].PHP_EOL;
+                $attrStr .= $attr['name'] . ": " . $attr['value'] . PHP_EOL;
             }
 
             array_push($orderDetailArray['type'], $cart->service_category);
-            array_push($orderDetailArray['name'],$cart->service_name);
+            array_push($orderDetailArray['name'], $cart->service_name);
             array_push($orderDetailArray['price'], $cart->service->price);
-            array_push($orderDetailArray['attr'],$attrStr);
+            array_push($orderDetailArray['attr'], $attrStr);
             $orderDetailArray['total'] += $cart->service_price;
         }
-        
+
 
         return $orderDetailArray;
     }
